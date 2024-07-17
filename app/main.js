c79181adce67bd74202481ce69493b5e584638ff
naman360 <naman.geek@gmail.com>
2024-07-17 23:55:42 +0530

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-
+const zlib = require("zlib");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
@@ -10,6 +10,10 @@ const command = process.argv[2];
 switch (command) {
   case "init":
     createGitDirectory();
+    break;
+  case "cat-file":
+    const hash = process.argv[4];
+    catFile(hash);
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -26,5 +30,20 @@ function createGitDirectory() {
     path.join(process.cwd(), ".git", "HEAD"),
     "ref: refs/heads/main\n"
   );
+
   console.log("Initialized git directory");
+  function catFile(hash) {
+    const content = fs.readFileSync(
+      path.join(
+        process.cwd(),
+        ".git",
+        "objects",
+        hash.slice(0, 2),
+        hash.slice(2)
+      )
+    );
+    const decompressedData = zlib.inflateSync(content);
+    const output = decompressedData.split("\0")[1];
+    process.stdout.write(output);
+  }
 }
